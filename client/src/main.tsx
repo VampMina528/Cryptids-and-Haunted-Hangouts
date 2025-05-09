b320254e4f00fb41376e3b1b689d27275c3eac49
Mina <TMDHammond@outlook.com>
2025-05-08 19:32:10 -0700

    Updated main.tsx and CryptidDetail.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,1 +1,22 @@
-// Entry point(ReactDom render)
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import App from './App'; // Optional if used
+import HomePage from './pages/HomePage';
+import CryptidDetail from './pages/CryptidDetail';
+import client from './graphql/apolloClient';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cryptid/:id" element={<CryptidDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
+  </StrictMode>
+);
