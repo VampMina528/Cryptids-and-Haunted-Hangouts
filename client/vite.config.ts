375ae992a37efc12a45a0bdaa63badd64bfc07b6
dlwortmann <dannywortmann@gmail.com>
2025-05-13 18:23:13 -0500

    structure and root package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.', // use the root as base directory
+  build: {
+    outDir: '../client/dist', // Default — just ensure this exists
+  }
 })
