82f0775794d98b96a165c1409d80a28abaa0fbd6
Falmanza94 <fischer.almanza@gmail.com>
2025-05-20 18:12:42 -0500

    Add forum typeDefs and install graphql for ApolloV4

--- server/src/schemas/typeDefs/forumTypeDefs.ts
+++ server/src/schemas/typeDefs/forumTypeDefs.ts
@@ -1,1 +1,20 @@
-// TypeDefs for forum posts
+import { gql } from "graphql-tag";
+
+const forumTypeDefs = gql`
+    type ForumPost {
+        id: ID!
+        codename: String!
+        content: String!
+        createdAt: String!
+    }
+    
+    type Query {
+        getAllForumPosts: [ForumPost]
+    }
+    
+    type Mutation {
+        addForumPost(codename: String!, content: String!): ForumPost
+    }
+`;
+
+export default forumTypeDefs;
