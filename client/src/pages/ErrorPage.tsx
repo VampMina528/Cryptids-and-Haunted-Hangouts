8d71f2a089a4d7c4611d17716f8522b59650bf9f
Falmanza94 <fischer.almanza@gmail.com>
2025-05-21 20:53:21 -0500

    added error page w/out image in assets* will add later

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+export default function ErrorPage() {
+    return (
+      <div className="broken-glass-background">
+        <div className="broken-glass-content">
+          <h1>Oh no! 404</h1>
+          <a href="/" className="btn">Go Back Home</a>
+        </div>
+      </div>
+    );
+  }
+  
