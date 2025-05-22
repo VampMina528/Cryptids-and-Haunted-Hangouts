5ea77f4ae93d5cd570dfe5b368dadca901b271a1
Falmanza94 <fischer.almanza@gmail.com>
2025-05-21 19:04:17 -0500

    forumQueries.ts initialized

--- client/src/graphql/forumQueries.ts
+++ client/src/graphql/forumQueries.ts
@@ -1,1 +1,23 @@
-// GraphQL operations
+import { gql } from "@apollo/client";
+
+export const GET_ALL_FORUMS_POSTS = gql`
+    query GetAllForumPosts {
+        getAllForumPosts {
+            id
+            codename
+            content
+            createdAt
+        }
+    }
+`;
+
+export const ADD_FORUM_POST = gql`
+    mutation AddForumPost($codename: String!, $content: String!) {
+        addForumPost(codename: $codename, content: $content) {
+            id
+            codename
+            conent
+            createdAt
+        }
+    }
+`;
